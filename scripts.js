d6a5c6bd392a47409efd953989b4e6a841c88f21
Thaddaeus Fam <thaddaeusfamous@gmail.com>
2022-09-08 13:25:48 -0500

    modal

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
+const addBook = document.querySelector("#add-book-id");
+const modal = document.querySelector(".modal-container");
+const closeModal = document.querySelector(".close");
+addBook.addEventListener("click", showModal);
+
+function showModal() {
+	modal.classList.toggle("show");
+}
+
+closeModal.addEventListener("click", hideModal);
+function hideModal() {
+	modal.classList.toggle("show");
+}
+
 const library = [];
 
 function Book() {}
