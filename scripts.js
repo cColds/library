2f156e2570cc8581afd337d1e234cbbeba06fe7c
Thaddaeus Fam <thaddaeusfamous@gmail.com>
2022-09-09 01:53:26 -0500

    modal should be done

--- scripts.js
+++ scripts.js
@@ -1,16 +1,43 @@
 const addBook = document.querySelector("#add-book-id");
 const modal = document.querySelector(".modal-container");
 const closeModal = document.querySelector(".close");
+const bookTitle = document.querySelector(".book-title");
+const bookAuthor = document.querySelector(".book-author");
+const bookPages = document.querySelector(".book-pages");
+const checkbox = document.querySelector(".book-read");
+const errorText = document.querySelector(".error");
+const submitButton = document.querySelector(".submit-button");
 addBook.addEventListener("click", showModal);
 
 function showModal() {
 	modal.classList.toggle("show");
 }
 
-closeModal.addEventListener("click", hideModal);
+closeModal.addEventListener("click", () => {
+	clearText();
+
+	hideModal();
+});
 function hideModal() {
 	modal.classList.toggle("show");
 }
+
+function isFormValid() {
+	if (bookTitle.value && bookAuthor.value && bookPages.value) {
+		hideModal();
+		clearText();
+	} else errorText.textContent = "*All fields must be filled";
+}
+
+function clearText() {
+	bookTitle.value = "";
+	bookAuthor.value = "";
+	bookPages.value = "";
+	errorText.textContent = "";
+	checkbox.checked = false;
+}
+
+submitButton.addEventListener("click", isFormValid);
 
 const library = [];
 
