7a966df430cbf7015c2408874d370dcaf31d3086
cColds <103373668+cColds@users.noreply.github.com>
2023-07-09 08:13:34 -0500

    Install and setup webpack

---
+++ src/index.js
@@ -0,0 +1,146 @@
+const addBook = document.querySelector("#add-book-id");
+const removeAllBooks = document.querySelector(".remove-all");
+const modal = document.querySelector(".modal-container");
+const closeModal = document.querySelector(".close");
+const bookTitle = document.querySelector(".book-title");
+const bookAuthor = document.querySelector(".book-author");
+const bookPages = document.querySelector(".book-pages");
+const isRead = document.querySelector(".book-read");
+const errorText = document.querySelector(".error");
+const submitButton = document.querySelector(".submit-button");
+const toggleLibraryText = document.querySelector(".library-info-container");
+
+function toggleModal() {
+  modal.classList.toggle("show");
+}
+
+function isFormValid() {
+  return (
+    bookTitle.validity.valid &&
+    bookAuthor.validity.valid &&
+    bookPages.validity.valid
+  );
+}
+
+function clearValues() {
+  bookTitle.value = "";
+  bookAuthor.value = "";
+  bookPages.value = "";
+  errorText.textContent = "⠀";
+  isRead.checked = false;
+}
+
+function updateReadCardClassNameAndTextContent(bookIsRead, readCard) {
+  if (bookIsRead) {
+    readCard.textContent = "Read";
+    readCard.classList.remove("not-read");
+  } else {
+    readCard.classList.add("not-read");
+    readCard.textContent = "Not Read";
+  }
+}
+
+addBook.addEventListener("click", () => {
+  toggleModal();
+  clearValues();
+});
+
+closeModal.addEventListener("click", toggleModal);
+submitButton.addEventListener("click", () => {
+  if (!isFormValid()) {
+    errorText.textContent = "*All fields must be filled";
+    return;
+  }
+
+  addBookToLibrary();
+  toggleModal();
+});
+
+const library = [];
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+function addBookToLibrary() {
+  const book = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPages.value,
+    isRead.checked
+  );
+  library.push(book);
+  toggleLibraryText.classList.add("hide");
+  loopLibrary();
+}
+
+function getBookIndex(e) {
+  const { indexNumber } = e.target.closest("[data-index-number]").dataset;
+  return Number(indexNumber);
+}
+
+function loopLibrary() {
+  const cardContainer = document.querySelector(".card-container");
+  cardContainer.innerHTML = "";
+  let index = -1;
+
+  library.forEach((book) => {
+    const card = document.createElement("div");
+    const removeCard = document.createElement("button");
+    const cardInfo = document.createElement("div");
+    const titleCard = document.createElement("div");
+    const authorCard = document.createElement("div");
+    const pagesCard = document.createElement("div");
+    const readCard = document.createElement("button");
+
+    card.dataset.indexNumber = `${(index += 1)}`;
+
+    cardInfo.classList.add("card-info");
+    removeCard.classList.add("remove-card");
+
+    titleCard.classList.add("title-card");
+    authorCard.classList.add("author-card");
+    pagesCard.classList.add("pages-card");
+    readCard.classList.add("read-card");
+    card.classList.add("card");
+
+    removeCard.textContent = "❌";
+    titleCard.textContent = book.title;
+    authorCard.textContent = `by ${book.author}`;
+    pagesCard.textContent = `${book.pages} pages`;
+    updateReadCardClassNameAndTextContent(book.read, readCard);
+
+    readCard.addEventListener("click", (e) => {
+      const bookIndex = getBookIndex(e);
+
+      library[bookIndex].read = !library[bookIndex].read;
+      updateReadCardClassNameAndTextContent(book.read, readCard);
+    });
+
+    removeCard.addEventListener("click", (e) => {
+      const bookIndex = getBookIndex(e);
+
+      library.splice(bookIndex, 1);
+      card.remove();
+      card.innerHTML = "";
+
+      if (!library.length) toggleLibraryText.classList.remove("hide");
+
+      loopLibrary();
+    });
+
+    removeAllBooks.addEventListener("click", () => {
+      library.length = 0;
+      cardContainer.innerHTML = "";
+      toggleLibraryText.classList.remove("hide");
+    });
+
+    cardInfo.append(titleCard, authorCard, pagesCard, readCard);
+    card.append(removeCard, cardInfo);
+    cardContainer.append(card);
+  });
+}
