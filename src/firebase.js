a017c8b152ccb57b5dcc262738dc44b7cff05225
cColds <103373668+cColds@users.noreply.github.com>
2023-07-10 07:00:20 -0500

    Initialize and add config firebase auth

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "@firebase/app";
+import { getAuth } from "@firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyAEVQPK27XMkodcV4MVzAngFdnj3TeFYYk",
+  authDomain: "library-ea366.firebaseapp.com",
+  projectId: "library-ea366",
+  storageBucket: "library-ea366.appspot.com",
+  messagingSenderId: "1054822464232",
+  appId: "1:1054822464232:web:e078150f99bbb03dc8a75d",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
