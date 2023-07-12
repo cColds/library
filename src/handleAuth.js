530fbf1a729c72884aa282c281df00885172bdf0
cColds <103373668+cColds@users.noreply.github.com>
2023-07-11 19:16:33 -0500

    Add user to users collection firestore

--- src/handleAuth.js
+++ src/handleAuth.js
@@ -5,12 +5,17 @@ import {
   signOut,
   getAuth,
 } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "@firebase/firestore";
 import app from "./firebase";
 
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, async (user) => {
   if (user) {
+    const docRef = doc(db, `users/${user.uid}`);
+    const docSnap = await getDoc(docRef);
+    if (!docSnap.exists()) await setDoc(doc(db, docRef.path), {});
     login.classList.remove("active");
     logout.classList.add("active");
   } else {
@@ -25,7 +30,7 @@ async function handleLogin() {
   try {
     const provider = new GoogleAuthProvider();
     const result = await signInWithPopup(auth, provider);
-    console.log(result);
+    console.log("Logged in with result:", result);
   } catch (error) {
     console.error(error);
   }
